7fe4b12c775a022ccfa4fd4f1fed4b7f89397d99
ducnv102 <ducduc1022000@gmail.com>
2025-11-25 22:44:56 +0700

    update: ghep api ds chuyen khoa

--- src/app/[lang]/chuyen-khoa/page.tsx
+++ src/app/[lang]/chuyen-khoa/page.tsx
@@ -1,20 +1,48 @@
 import DepartmentDetailPage from '@/src/components/departments/DepartmentDetailPage';
 import LogoSlider from '@/src/components/sections/slider/LogoSlider';
 import { getAllDepartmentGroups } from '@/src/services/department';
+import { fnGetPageBySlug } from '@/src/services/page';
+import { createSeoData } from '@/src/utils/metadata';
+import { ResolvingMetadata, Metadata } from 'next';
+import { cookies } from 'next/headers';
 
-type Props = {};
+async function getLangSlug(): Promise<string> {
+  const cookieStore = await cookies();
+  const lang = cookieStore.get('language')?.value ?? 'vi';
+  return lang === 'en' ? 'chuyen-khoa-en' : 'chuyen-khoa';
+}
 
-const DepartmentListPageWrapper = async (props: Props) => {
+export async function generateMetadata(
+  _props: any,
+  _parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const langSlug = await getLangSlug();
+  const data = await fnGetPageBySlug(langSlug);
+  const seo = createSeoData(data?.seo) ?? {};
+  return seo;
+}
+
+const DepartmentListPageWrapper = async () => {
+  const langSlug = await getLangSlug();
+  const pageContent = await fnGetPageBySlug(langSlug);
+
+  const pageSchema = pageContent?.seo?.meta_schema;
+
   const departmentGroups = await getAllDepartmentGroups();
   const parentGroups = departmentGroups?.filter((d) => d.parent_group === null);
+
+  const sections = pageContent?.sections;
+  const bannerData = sections.find((s: any) => s.type === 'hero-text-overlay');
+  const logoSlider = sections.find((s: any) => s.type === 'logo-slider');
 
   return (
     <div>
       <DepartmentDetailPage
+        bannerData={bannerData}
         departmentGroups={departmentGroups as any[]}
         parentGroups={parentGroups as any[]}
       />
-      <LogoSlider data={LogoSliderData} />
+      <LogoSlider data={logoSlider} />
     </div>
   );
 };
