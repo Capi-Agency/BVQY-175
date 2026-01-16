f9d1590dc9633f81ead456f8da4ab8b964770cf3
AnhTD <dt14893@gmail.com>
2026-01-17 00:04:35 +0700

    test deploy

--- src/components/sections/card/CardSliderWithLeftRightButton.tsx
+++ src/components/sections/card/CardSliderWithLeftRightButton.tsx
@@ -16,6 +16,7 @@ export default function CardSliderWithLeftRightButton({ data }: CommonSection) {
   const mounted = useMounted();
 
   if (!mounted) return null;
+  
 
   return (
     <section className="flex flex-col items-stretch gap-6 bg-[#F6FAF7] py-10 md:gap-8 md:py-6 lg:py-10 xl:gap-11 xl:py-11 2xl:gap-12 2xl:py-12 3xl:gap-[52px] 3xl:py-[52px] 4xl:gap-[60px] 4xl:py-[60px]">
