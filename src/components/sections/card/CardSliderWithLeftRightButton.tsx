968d140a2c41923704ff087196bf462e16d0709e
ducnv102 <ducduc1022000@gmail.com>
2026-01-16 21:44:12 +0700

    fix hydration error

--- src/components/sections/card/CardSliderWithLeftRightButton.tsx
+++ src/components/sections/card/CardSliderWithLeftRightButton.tsx
@@ -9,9 +9,13 @@ import NextImg from '../../common/next-img';
 import { getAssetUrlById } from '@/src/utils/image';
 import CustomLink from '../../common/custom-link';
 import { useTranslations } from 'next-intl';
+import { useMounted } from '@/src/hooks/use-mounted';
 
 export default function CardSliderWithLeftRightButton({ data }: CommonSection) {
   const t = useTranslations('Doctor');
+  const mounted = useMounted();
+
+  if (!mounted) return null;
 
   return (
     <section className="flex flex-col items-stretch gap-6 bg-[#F6FAF7] py-10 md:gap-8 md:py-6 lg:py-10 xl:gap-11 xl:py-11 2xl:gap-12 2xl:py-12 3xl:gap-[52px] 3xl:py-[52px] 4xl:gap-[60px] 4xl:py-[60px]">
@@ -23,9 +27,7 @@ export default function CardSliderWithLeftRightButton({ data }: CommonSection) {
             </p>
           )}
           {data?.title && (
-            <h2 className="section-title text-primary-600">
-              {data?.title}
-            </h2>
+            <h2 className="section-title text-primary-600">{data?.title}</h2>
           )}
         </div>
 
--- src/services/page.ts
+++ src/services/page.ts
@@ -15,9 +15,11 @@ export const fnGetPageBySlug = async (slug: string) => {
     return res?.raw_content ?? null;
   } catch (error: any) {
     console.log(
-      'Error getting page content: ',
+      'Error in fnGetPageBySlug: ',
       error?.errors?.[0]?.message || error?.message || error,
     );
+    console.log('slug:', slug);
+    console.log('============');
     return null;
   }
 };
@@ -53,9 +55,9 @@ export const fnGetAllPageSlug = async () => {
         fields: ['slug', 'language'],
         filter: {
           slug: {
-            _nistarts_with: 'chi-tiet'
-          }
-        }
+            _nistarts_with: 'chi-tiet',
+          },
+        },
       }),
     );
 
