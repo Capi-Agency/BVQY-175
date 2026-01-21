005f0a947a5901e37305d2b113d6f213d4d87131
ducnv <133721410+ducnguyen1002@users.noreply.github.com>
2026-01-21 15:10:08 +0700

    update department card

--- src/components/departments/DepartmentCard.tsx
+++ src/components/departments/DepartmentCard.tsx
@@ -53,31 +53,35 @@ export default function DepartmentCard({
         </div>
 
         <div className="space-y-1">
-          <div className="text-lg font-semibold !leading-[1.5] text-primary-1000 duration-200 group-hover:text-primary-50 xl:text-xl 2xl:line-clamp-2 2xl:h-16 3xl:h-[66px] 3xl:text-[22px] 4xl:h-[72px] 4xl:text-2xl">
+          {/* TITLE */}
+          <div className="line-clamp-3 h-[60px] text-base font-semibold !leading-[1.25] text-primary-1000 duration-200 group-hover:text-primary-50 xl:h-[67.5px] xl:text-lg 2xl:h-[67.5px] 3xl:h-[75px] 3xl:text-[20px] 4xl:h-[75px] 4xl:text-xl">
             {item?.title} {item?.code && ` (${item.code})`}
           </div>
-          <div
-            className="line-clamp-3 text-sm 3xl:text-base font-thin text-[#03110899] duration-200 group-hover:text-primary-100"
+
+          {/* BLURB */}
+          {/* <div
+            className="line-clamp-3 text-xs font-thin text-[#03110899] duration-200 group-hover:text-primary-100 3xl:text-sm"
             dangerouslySetInnerHTML={{
               __html: item?.[blurb],
             }}
-          ></div>
+          /> */}
         </div>
       </div>
 
-      <div className="flex justify-between">
+      {/* <div className="flex justify-between">
         <div className="flex items-center gap-1.5">
-          <span className="text-sm font-medium text-gray-950 duration-200 group-hover:text-primary-50 2xl:text-base 3xl:text-lg">
+          <span className="text-xs font-medium text-gray-950 duration-200 group-hover:text-primary-50 2xl:text-sm 3xl:text-base">
             {t('view-detail')}
           </span>
-          <div className="relative size-5 transition-all duration-200 group-hover:brightness-0 group-hover:invert 2xl:size-6">
+
+          <div className="relative size-4 transition-all duration-200 group-hover:brightness-0 group-hover:invert 2xl:size-5">
             <NextImg
               src="/assets/icons/arrow_right_black.svg"
               alt="arrow icon"
             />
           </div>
         </div>
-      </div>
+      </div> */}
     </CustomLink>
   );
 
