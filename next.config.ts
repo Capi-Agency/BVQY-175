e303ccb62c7ab7b0e2c4c4e6cbcc8c6e4f2203be
ducnv <133721410+ducnguyen1002@users.noreply.github.com>
2026-02-09 16:25:37 +0700

    update middleware to remove rsc param

--- next.config.ts
+++ next.config.ts
@@ -34,7 +34,6 @@ const nextConfig: NextConfig = {
   },
   compiler: {
     styledComponents: true,
-    removeConsole: process.env.NODE_ENV === 'production',
   },
   compress: true,
   devIndicators: false,
