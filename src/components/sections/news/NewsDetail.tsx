6ff12ae13b44c0125268688da4dacb1e1e165a7c
ducnv <133721410+ducnguyen1002@users.noreply.github.com>
2026-02-11 09:44:33 +0700

    remove log

--- src/components/sections/news/NewsDetail.tsx
+++ src/components/sections/news/NewsDetail.tsx
@@ -36,8 +36,6 @@ export default function NewsDetail({ data, dataDetail }: CommonSection) {
     });
   }, [dataDetail]);
 
-  console.log(dataDetail);
-
   return (
     <section className="container my-10 lg:my-12 2xl:my-[72px] 3xl:my-20">
       <div className="mt-6 flex flex-col gap-6 md:grid md:grid-cols-[auto,220px] md:flex-row lg:mt-10 lg:grid-cols-[auto,260px] lg:gap-8 lg:px-6 xl:gap-11 xl:px-[60px] 2xl:gap-12 2xl:px-[100px] 3xl:gap-[60px] 3xl:px-[80px] 4xl:mt-[60px] 4xl:px-[160px]">
