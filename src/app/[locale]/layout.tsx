ada9f37553502a01cc594d8207f4c5c08e5007aa
Nguyễn Văn Đức <133721410+ducnguyen1002@users.noreply.github.com>
2026-07-12 20:22:11 +0700

    add GA4

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -128,6 +128,18 @@ export default async function RootLayout({
           `}
         </Script>
         {/* End EMC Tracking Code */}
+
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-RD3PNBM6T2" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-RD3PNBM6T2');
+          `}
+        </Script>
       </head>
 
       <body className="antialiased">
